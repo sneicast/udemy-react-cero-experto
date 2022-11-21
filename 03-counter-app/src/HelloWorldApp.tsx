45b08a4d32ba0e45d2faad675c1540f06a60ce6c
Sneider Castillo <sneicast@hotmail.com>
2022-11-20 19:26:30 -0500

    Primer componente

---
+++ 03-counter-app/src/HelloWorldApp.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
--- 03-counter-app/src/main.tsx
+++ 03-counter-app/src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
 
-function App(){
-    return (<h1>Hola Mundo!!!</h1>);
-}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <HelloWorldApp/>
     </React.StrictMode>
 );
