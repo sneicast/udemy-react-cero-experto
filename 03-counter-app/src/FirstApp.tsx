260967ececbc9599ac48f760a6c03fbfab03cfff
Sneider Castillo <sneicast@hotmail.com>
2022-11-20 19:34:34 -0500

    Retornar elementos en el componente Fragment

---
+++ 03-counter-app/src/FirstApp.tsx
@@ -0,0 +1,10 @@
+// import { Fragment } from "react"
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>Sneider Castillo</h1>
+            <p>Soy un Subtitulo</p>
+        </>
+    )
+}
--- 03-counter-app/src/HelloWorldApp.tsx
+++ 03-counter-app/src/HelloWorldApp.tsx
@@ -1,6 +1,4 @@
 
-import React from 'react'
-
 export const HelloWorldApp = () => {
   return (
     <h1>HelloWorldApp</h1>
