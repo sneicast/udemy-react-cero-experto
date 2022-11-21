a2dbe8d90cfb144c496492fa24d4da8230871896
Sneider Castillo <sneicast@hotmail.com>
2022-11-20 19:51:27 -0500

    Impresion de variables en HTML

--- 03-counter-app/src/FirstApp.tsx
+++ 03-counter-app/src/FirstApp.tsx
@@ -1,9 +1,19 @@
 // import { Fragment } from "react"
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Fernando'
+};
+
+const getResul =(a: number, b: number) => {
+    return a + b;
+}
 
 export const FirstApp = () => {
+    
     return (
         <>
-            <h1>Sneider Castillo</h1>
+            <h1>{(getResul(4, 7))}</h1>
+            {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>Soy un Subtitulo</p>
         </>
     )
