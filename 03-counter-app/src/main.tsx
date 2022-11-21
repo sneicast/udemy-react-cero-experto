fa6301689a8384343e65aa2f324c5045c3c28c03
Sneider Castillo <sneicast@hotmail.com>
2022-11-20 19:17:36 -0500

    Hola mundo

--- 03-counter-app/src/main.tsx
+++ 03-counter-app/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App(){
+    return (<h1>Hola Mundo!!!</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
